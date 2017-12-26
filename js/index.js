da43c8462752b87e0b90f435593e0b8edc9bde4b
xuteng <xutxxx@163.com>
2017-12-26 18:00:52 +0800

    1

--- js/index.js
+++ js/index.js
@@ -1,15 +1,38 @@
-;$(function(){
-	$('.container').on('touchstart', '.item', function(event) {
-		$(this).css({
-			opacity: '.2'
-		})
-		setTimeout(function(){
-			$(this).removeAttr('style')
-		}.bind(this),500)
-		snsEvent.trigger('randomSounds')
-	});
-})
 
+var active = {
+	curDoms: [],
+	domEvents: function() {
+		var that = this
+		window.addEventListener('touchmove', function(event) {
+			$(event.target).addClass('active')
+			console.log(event.touches)
+			// that.domAdd(event.target)
+		},false)
+		// window.addEventListener('touchstart', function(event) {
+		// 	$(event.target).addClass('active')
+		// 	// that.domAdd(event.target)
+		// },false)
+	},
+	domAdd:function(dom){
+		this.curDoms.push(dom)
+		console.log(this.curDoms)
+	},
+	domDel:function(){
+		var that = this;
+		function timer(){
+			if(that.curDoms.length!==0){
+				$(that.curDoms.shift()).removeClass('active')
+			}
+			setTimeout(timer,1000);
+		}
+		timer();
+	}
+};
 snsEvent.listen('randomSounds',function(){
 	console.log('bofang')
+});
+
+;$(function(){
+	active.domEvents();
+	active.domDel();
 })
